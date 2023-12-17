8bd2bf2f46fd1150da8043d776982d968c981493
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2023-12-17 12:45:11 +0530

    markup of Login page

--- InstagramLogin/components/SignIn/SignIn.jsx
+++ InstagramLogin/components/SignIn/SignIn.jsx
@@ -1,6 +1,18 @@
 import './SignIn.css'
 
 function SignIn(){
-
+    return(
+        <div className='LoginCard'>
+            <div>
+                <img src='./Logo.png' alt='Logo'/>
+            </div>
+            <div>
+                <input type='text' placeholder='Phone number, username or email'/>
+                <input type='password' placeholder='Password'/>
+            </div>
+            <div>Sign in</div>
+            <div>Don't have an account? <span>Sign up</span></div>
+        </div>
+    )
 }
 export default SignIn;
--- InstagramLogin/components/SignUp/SignUp.jsx
+++ InstagramLogin/components/SignUp/SignUp.jsx
@@ -1,6 +1,20 @@
 import './SignUp.css'
 
 function SignUp(){
-
+    return(
+        <div>
+            <div>
+                <img src='./Logo.png' alt='Logo'/>
+            </div>
+            <div>
+                <input type='text' placeholder='Mobile Number'/>
+                <input type='text' placeholder='Full Name'/>
+                <input type='email' placeholder='email'/>
+                <input type='password' placeholder='Password'/>
+            </div>
+            <div>Sign un</div>
+            <div>Have an account? <span>Log in</span></div>
+        </div>
+    )
 }
 export default SignUp;
