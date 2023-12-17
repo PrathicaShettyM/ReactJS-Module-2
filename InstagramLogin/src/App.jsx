ac885758fc03d0b43afe19a139cc0141d5e82a08
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2023-12-17 14:02:37 +0530

    Output

--- InstagramLogin/src/App.jsx
+++ InstagramLogin/src/App.jsx
@@ -7,28 +7,33 @@ function App() {
   function Change(){
     setLogin(!login)
   }
+
   return(
     <div className='LoginCard'>
+
       <div className='img'>
         <img 
             src='https://assets.stickpng.com/images/5a4e432a2da5ad73df7efe7a.png' 
             height="100px" 
             alt='Logo'/>
       </div>
+
       <div className='inputFields'>
         <input hidden={login} className='input' type='text' placeholder='Mobile Number'/>
         <input hidden={login} className='input' type='text' placeholder='Full Name'/>
         <input className='input' type='email' placeholder='Email'/>
         <input className='input' type='password' placeholder='Password'/>
       </div>
+
       <div className='btn'>{login?" SignIn ":" SignUp "}</div>
-      
+
         <div className='link'>
           {login?"Don't have an account?":"Have an account?"}
             <span onClick={Change}>
               {login?"Sign Up":"Sign In"}
             </span>
         </div>
+        
     </div>
   )
 
