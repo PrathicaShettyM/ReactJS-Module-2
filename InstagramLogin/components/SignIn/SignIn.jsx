99389c387e490c1b54474ecfcdf488ea6d6b727f
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2023-12-17 13:10:50 +0530

    designing UI of Login page

--- InstagramLogin/components/SignIn/SignIn.jsx
+++ InstagramLogin/components/SignIn/SignIn.jsx
@@ -3,15 +3,17 @@ import './SignIn.css'
 function SignIn(){
     return(
         <div className='LoginCard'>
-            <div>
-                <img src='./Logo.png' alt='Logo'/>
+            <div className='img'>
+                <img 
+                    src='https://assets.stickpng.com/images/5a4e432a2da5ad73df7efe7a.png' height="100px" 
+                    alt='Logo'/>
             </div>
-            <div>
+            <div className='inputFields'>
                 <input type='text' placeholder='Phone number, username or email'/>
                 <input type='password' placeholder='Password'/>
             </div>
-            <div>Sign in</div>
-            <div>Don't have an account? <span>Sign up</span></div>
+            <div className='btn'>Sign in</div>
+            <div className='link'>Don't have an account? <span>Sign up</span></div>
         </div>
     )
 }
--- InstagramLogin/components/SignUp/SignUp.jsx
+++ InstagramLogin/components/SignUp/SignUp.jsx
@@ -2,18 +2,21 @@ import './SignUp.css'
 
 function SignUp(){
     return(
-        <div>
-            <div>
-                <img src='./Logo.png' alt='Logo'/>
+        <div className='LoginCard'>
+            <div className='img'>
+                <img 
+                    src='https://assets.stickpng.com/images/5a4e432a2da5ad73df7efe7a.png' 
+                    height="100px" 
+                    alt='Logo'/>
             </div>
-            <div>
+            <div className='inputFields'>
                 <input type='text' placeholder='Mobile Number'/>
                 <input type='text' placeholder='Full Name'/>
                 <input type='email' placeholder='email'/>
                 <input type='password' placeholder='Password'/>
             </div>
-            <div>Sign un</div>
-            <div>Have an account? <span>Log in</span></div>
+            <div className='btn'>Sign up</div>
+            <div className='link'>Have an account? <span>Log in</span></div>
         </div>
     )
 }
