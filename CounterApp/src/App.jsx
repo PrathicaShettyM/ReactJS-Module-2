b03567894a01c38d445f16c5008b4328d613a542
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2023-12-17 11:54:26 +0530

    Counter App done

--- CounterApp/src/App.jsx
+++ CounterApp/src/App.jsx
@@ -5,16 +5,21 @@ function App() {
   const [count, setCount] = useState(0);
 
   function Increment(){
-    console.log("Increment")
+    console.log("Increment");
+    setCount(count+1)
   }
   function Decrement(){
-    console.log("Decrement")
+    console.log("Decrement");
+    setCount(count-1);
   }
   return (
-    <>
-      <button onClick={Increment}>Increment</button>
-      <button></button>
-    </>
+    <div className='CounterCard'>
+      <div className='Count'>{count}</div>
+      <div className='Btns'>
+        <button className='btn' onClick={Increment}>Increment</button>
+        <button className='btn' onClick={Decrement}>Decrement</button>
+      </div> 
+    </div>
   )
 }
 
