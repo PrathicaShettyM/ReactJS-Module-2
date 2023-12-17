b82682efd91b69facad7d0b3c92047796f2e3d9e
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2023-12-17 14:00:06 +0530

    conditional rendering in Login Page

--- InstagramLogin/src/App.jsx
+++ InstagramLogin/src/App.jsx
@@ -1,14 +1,37 @@
+import { useState } from 'react'
 import './App.css'
-import SignIn from '../components/SignIn/SignIn'
-import SignUp from '../components/SignUp/SignUp'
 
 function App() {
-  return (
-    <>
-      <SignIn/>
-      <SignUp/>
-    </>
+  const [login, setLogin] = useState(true);
+
+  function Change(){
+    setLogin(!login)
+  }
+  return(
+    <div className='LoginCard'>
+      <div className='img'>
+        <img 
+            src='https://assets.stickpng.com/images/5a4e432a2da5ad73df7efe7a.png' 
+            height="100px" 
+            alt='Logo'/>
+      </div>
+      <div className='inputFields'>
+        <input hidden={login} className='input' type='text' placeholder='Mobile Number'/>
+        <input hidden={login} className='input' type='text' placeholder='Full Name'/>
+        <input className='input' type='email' placeholder='Email'/>
+        <input className='input' type='password' placeholder='Password'/>
+      </div>
+      <div className='btn'>{login?" SignIn ":" SignUp "}</div>
+      
+        <div className='link'>
+          {login?"Don't have an account?":"Have an account?"}
+            <span onClick={Change}>
+              {login?"Sign Up":"Sign In"}
+            </span>
+        </div>
+    </div>
   )
+
 }
 
 export default App
